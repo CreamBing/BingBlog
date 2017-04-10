8e34930034107ad7165feb16c1800d051e19cb50
zhaobing <CreamBing1994@gmail.com>
2017-04-10 21:25:10 +0800

    [t][bingblog项目初始化][ssm+log4j2][bingcream]

---
+++ src/main/java/com/bing/bingblog/busi/service/TestLog4j2Service.java
@@ -0,0 +1,23 @@
+package com.bing.bingblog.busi.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 测试一下log4j2的配置
+ * Created by zhaobing on 2017/4/10.
+ */
+@Service
+public class TestLog4j2Service {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestLog4j2Service.class);
+
+    public void testLog4j2(){
+        LOG.trace("1、我是TestLog4j2Service trace信息");
+        LOG.debug("2、我是TestLog4j2Service debug信息");
+        LOG.info("3、我是TestLog4j2Service info信息");
+        LOG.warn("4、我是TestLog4j2Service warn信息");
+        LOG.error("5、我是TestLog4j2Service error信息");
+    }
+}
--- src/main/java/com/bing/bingblog/web/ctrl/IndexController.java
+++ src/main/java/com/bing/bingblog/web/ctrl/IndexController.java
@@ -1,7 +1,9 @@
 package com.bing.bingblog.web.ctrl;
 
+import com.bing.bingblog.busi.service.TestLog4j2Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,13 +17,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
 
     private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
+    @Autowired
+    private TestLog4j2Service testLog4j2Service;
 
     @RequestMapping(value = "index", method = RequestMethod.GET, produces = "application/json")
     public String toIndex() {
-        LOG.debug("debug");
-        LOG.info("info");
-        LOG.warn("warn");
-        LOG.error("error");
+        LOG.trace("1、我是trace信息{}{}","用户信息","业务代码");
+        LOG.debug("2、我是debug信息");
+        LOG.info("3、我是info信息");
+        LOG.warn("4、我是warn信息");
+        LOG.error("5、我是error信息");
+        testLog4j2Service.testLog4j2();
         return "index/index";
     }
 }
